fd05a4142c6a1c8c4d06293dd36eabfaca47a926
Jörgen Brandt <joergen@cuneiform-lang.org>
2024-10-02 23:52:25 +0200

    fix dependencies

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection "soap")
-(define deps '("base"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define deps '("base" "typed-racket-lib" "typed-racket-more"))
+(define build-deps '("scribble-lib" "racket-doc" "typed-racket-lib" "rackunit-typed"))
 (define scribblings '(("scribblings/soap.scrbl" ())))
 (define pkg-desc "SOA in Racket")
 (define version "0.1")
